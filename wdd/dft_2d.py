acec36f0e289a2fb342e3e7d454132937fbfa15c
Patrick Huang <huangpatrick16777216@gmail.com>
2025-04-03 19:28:23 -0500

    Apply high pass filter to input to 2D dft.

--- wdd/dft_2d.py
+++ wdd/dft_2d.py
@@ -8,26 +8,31 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy
 
 from perpixel_dft import get_sample, compute_dft
 
 
-def compute_spatial_dft(dft, freq_max, bins, omit_thres=math.pi / 15):
+def compute_spatial_dft(img, freq_max, bins):
+    # Normalize data
+    img -= img.mean()
+    img /= img.std()
+
+    # High pass filter
+    img = img - scipy.ndimage.gaussian_filter(img, 3)
+
     res = np.zeros((bins, bins), dtype=float)
-    x, y = np.meshgrid(np.arange(dft.shape[1]), np.arange(dft.shape[0]))
+    x, y = np.meshgrid(np.arange(img.shape[1]), np.arange(img.shape[0]))
 
     for i in range(bins):
         for j in range(bins):
             w_x = np.interp(j, (0, bins), (-freq_max, freq_max))
             w_y = np.interp(i, (0, bins), (-freq_max, freq_max))
-            if abs(w_x) < omit_thres and abs(w_y) < omit_thres:
-                # Omit low freqs.
-                continue
 
             theta = x * w_x + y * w_y
             out = np.hypot(
-                (dft * np.cos(theta)).mean(),
-                (dft * np.sin(theta)).mean(),
+                (img * np.cos(theta)).mean(),
+                (img * np.sin(theta)).mean(),
             )
             res[i, j] = out
 
@@ -38,7 +43,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=Path, required=True)
     parser.add_argument("--freq_time", type=float, default=0.75)
-    parser.add_argument("--freq_space_max", type=float, default=math.pi)
+    parser.add_argument("--freq_space_max", type=float, default=math.pi / 2)
     parser.add_argument("--freq_space_bins", type=int, default=50)
     args = parser.parse_args()
 
