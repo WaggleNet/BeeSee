d293993211192892ce117e5b72614edee2cc3cc2
Patrick Huang <huangpatrick16777216@gmail.com>
2025-05-07 14:04:45 -0500

    Picam integration.

--- scripts/test_thorax.py
+++ scripts/test_thorax.py
@@ -4,6 +4,7 @@ Run thorax detection on frames of a video and save result.
 
 import os
 import sys
+import time
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src"))
 
 import argparse
@@ -13,8 +14,13 @@ import numpy as np
 import torch
 import torchvision.transforms.functional as F
 
-from model_dino import DinoNN
-from utils import DEVICE, extract_blobs, load_dino_model, preprocess_images
+from utils import extract_blobs, load_dino_model, preprocess_images
+
+COMPUTE_BLOBS = False
+USE_PICAM = False
+
+if USE_PICAM:
+    from picamera2 import Picamera2
 
 
 def run_thorax_model(model, frame):
@@ -57,35 +63,52 @@ def main():
 
     model = load_dino_model(args.model)
 
-    vid_path = 0 if args.video is None else args.video
-    video_read = cv2.VideoCapture(vid_path)
-    if vid_path == 0:
-        video_read.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+    # Setup camera or video stream.
+    if USE_PICAM:
+        print("Using PiCam.")
+        picam = Picamera2()
+        picam.configure(picam.create_preview_configuration(main={
+            "format": "RGB888",
+            "size": (1280, 720),
+        }))
+        picam.start()
+        time.sleep(1)
+
+    else:
+        vid_path = 0 if args.video is None else args.video
+        video_read = cv2.VideoCapture(vid_path)
+        if vid_path == 0:
+            video_read.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+        print("Using opencv video reader, path =", vid_path)
 
     while True:
-        ret, frame = video_read.read()
-        if not ret:
-            break
+        if USE_PICAM:
+            frame = picam.capture_array()
+        else:
+            ret, frame = video_read.read()
+            if not ret:
+                break
 
         frame, pred = run_thorax_model(model, frame)
         frame[pred] = (0, 255, 0)
 
-        """
-        blobs = extract_blobs(pred, 0.5)
-        print(f"Found {len(blobs)} blobs: ", end="")
-        for b in blobs:
-            # Extract COM
-            y, x = torch.where(b)
-            com = (x.float().mean(), y.float().mean())
-            print(f"{com} ", end="")
-        print()
-        """
+        if COMPUTE_BLOBS:
+            blobs = extract_blobs(pred, 0.5)
+            print(f"Found {len(blobs)} blobs: ", end="")
+            for b in blobs:
+                # Extract COM
+                y, x = torch.where(b)
+                com = (x.float().mean(), y.float().mean())
+                print(f"{com} ", end="")
+            print()
 
         cv2.imshow("a", frame)
         if cv2.waitKey(1) == ord("q"):
             break
 
-    video_read.release()
+    if not USE_PICAM:
+        video_read.release()
 
 
 if __name__ == "__main__":
